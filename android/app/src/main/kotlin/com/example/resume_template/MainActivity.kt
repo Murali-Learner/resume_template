6ac359a17fae31877f07a05ed34b04951936a5d4
Murali-Learner <muralikrishna2354@gmail.com>
2024-08-14 11:33:17 +0530

    'first'

---
+++ android/app/src/main/kotlin/com/example/resume_template/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.resume_template
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/resume_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_resume_template/flutter_resume_template.dart';
+import 'package:resume_template/data.dart';
+
+class MyResumePage extends StatelessWidget {
+  const MyResumePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Resume'),
+      ),
+      body: FlutterResumeTemplate(
+        data: data,
+        templateTheme: TemplateTheme.modern,
+        mode: TemplateMode.shakeEditAndSaveMode,
+        onSaveResume: (globalKey) async =>
+            await PdfHandler().createResume(globalKey),
+      ),
+    );
+  }
+}
